b65e764afc81aa0658e0eeefd3bae803323d5bac
Xochitl Menjivar <xandra.menji@gmail.com>
2018-05-31 03:35:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,5 +23,10 @@ def won?(board)
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
     
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    
   end
 end
